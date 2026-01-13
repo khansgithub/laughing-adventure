9c6d75c15e5127100a47383fdc43b1eff35fcbf8
khan <khan>
2026-01-13 22:09:52 +0000

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), viteSingleFile()],
+	build: {
+		cssCodeSplit: false,
+		rollupOptions: {
+			output: {
+				inlineDynamicImports: true,
+			},
+		},
+	},
 })
